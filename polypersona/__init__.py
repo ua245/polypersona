b099ca03dad4da0ed0755aab803dbd7e11482910
KaushikKC <kccreations1704@gmail.com>
2026-09-19 13:47:58 +0100

    Enable Logfire tracing on import

--- polypersona/__init__.py
+++ polypersona/__init__.py
@@ -7,3 +7,7 @@ load_dotenv(find_dotenv(usecwd=True))
 if os.environ.get("GEMINI_API_KEY"):
     os.environ["GOOGLE_API_KEY"] = os.environ.pop("GEMINI_API_KEY")
 os.environ.setdefault("PYDANTIC_AI_NO_BANNER", "1")
+
+from . import tracing  # noqa: E402 - needs the .env loaded above
+
+tracing.setup()
